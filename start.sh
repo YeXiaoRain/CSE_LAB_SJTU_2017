44369930d35a0f13727fd32867b90e78388e1ace
Dd Nirvana <ddnirvana1@gmail.com>
2017-10-12 20:11:48 +0800

    use stop_client instead of stop.sh in start.sh
    Signed-off-by: Dd Nirvana <ddnirvana1@gmail.com>

--- start.sh
+++ start.sh
@@ -38,14 +38,14 @@ sleep 2
 # make sure FUSE is mounted where we expect
 pwd=`pwd -P`
 if [ `mount | grep "$pwd/yfs1" | grep -v grep | wc -l` -ne 1 ]; then
-    sh stop.sh
+    sh stop_client.sh
     echo "Failed to mount YFS properly at ./yfs1"
     exit -1
 fi
 
 # make sure FUSE is mounted where we expect
 if [ `mount | grep "$pwd/yfs2" | grep -v grep | wc -l` -ne 1 ]; then
-    sh stop.sh
+    sh stop_client.sh
     echo "Failed to mount YFS properly at ./yfs2"
     exit -1
 fi
